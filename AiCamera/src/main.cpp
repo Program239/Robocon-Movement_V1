68ddf86623f9c21529efa7e673f5ee28ea5e7a6b
Program239 <thelordgamer239@gmail.com>
2025-06-14 04:16:33 +0800

    add server connection from app

--- AiCamera/src/main.cpp
+++ AiCamera/src/main.cpp
@@ -13,33 +13,27 @@ HUSKYLENS huskylens;
 int lastX = -1, lastY = -1;
 
 void setup() {
-  // Set your desired static IP, gateway, and subnet
-  IPAddress local_IP(192, 168, 4, 2);      // Change to your desired static IP
-  IPAddress gateway(192, 168, 4, 1);        // Usually your router or AP IP
-  IPAddress subnet(255, 255, 255, 0);       // Common subnet mask
 
-  WiFi.config(local_IP, gateway, subnet);
   Serial.begin(115200);
-  Wire.begin(21, 22);  // ESP32 default pins
-  WiFi.begin(ssid, password);
-
-  Serial.print("Connecting to " + String(ssid) + "...");
-  while (WiFi.status() != WL_CONNECTED) {
+  //Wire.begin(21, 22);  // ESP32 default pins
+  WiFi.softAP(ssid, password);
+  Serial.print("Initiating WiFi AP: ");
+  Serial.print(ssid);
+  while (WiFi.softAPgetStationNum() == 0) {
+    delay(100);
     Serial.print(".");
-    delay(500);
   }
-
-  Serial.println("\nConnected to WiFi!");
+  Serial.println("\nWiFi AP started successfully");
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.softAPIP());
   
   delay(100); // Let everything stabilize
-
+/*
   while (!huskylens.begin(Wire)) {
     Serial.println("Begin failed");
     delay(1000);  // Prevent watchdog resets
   }
-
+*/
   delay(1000); // Allow HuskyLens to initialize
 
 
@@ -54,12 +48,51 @@ void setup() {
     server.send(200, "text/plain", "ESP32 is running!");
   });
 
+  server.on("/net", []() {
+    int netDirection = server.arg("val").toInt();
+    if (netDirection == 1) {
+      Serial.println("Net direction: Up");
+    } else if (netDirection == -1) {
+      Serial.println("Net direction: Down");
+    } else {
+      Serial.println("Net direction: Stop");
+    }
+  });
+
+  server.on("/shooter", []() {
+    int shooterDirection = server.arg("direction").toInt();
+    if (shooterDirection == 1) {
+      Serial.println("Shooter direction: Right");
+    } else if (shooterDirection == -1) {
+      Serial.println("Shooter direction: Left");
+    } else {
+      Serial.println("Shooter direction: Stop");
+    }
+  });
+
+  server.on("/motor", []() { 
+    int motorState = server.arg("value").toInt();
+    if (motorState == 1) {
+      Serial.println("Motor state: ON");
+      // Add code to turn on the motor
+    } else if (motorState == 0) {
+      Serial.println("Motor state: OFF");
+      // Add code to turn off the motor
+    } else {
+      Serial.println("Invalid motor state");
+    }
+  });
+
+  server.on("/shoot", []() {
+    Serial.println("Shoot command received");      
+  });
+
   server.begin();
-  huskylens.writeAlgorithm(ALGORITHM_OBJECT_TRACKING);
+  //huskylens.writeAlgorithm(ALGORITHM_OBJECT_TRACKING);
 }
 
 void loop() {
-
+/*
   if (huskylens.request()) {
     if (huskylens.available()) {
       HUSKYLENSResult result = huskylens.read();
@@ -75,6 +108,6 @@ void loop() {
   } else {
   Serial.println("No data from HuskyLens");
   }
-
+*/
   server.handleClient();
 }
